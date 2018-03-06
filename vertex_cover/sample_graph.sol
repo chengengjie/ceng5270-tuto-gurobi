b618a4851e5d05d14246caadc3a40483a15d56d1
Gengjie Chen <chengengjie93@gmail.com>
2018-03-06 12:04:20 +0800

    Finish vertex cover

---
+++ vertex_cover/sample_graph.sol
@@ -0,0 +1,6 @@
+# Objective value = 2
+vertex0 0
+vertex1 0
+vertex2 1
+vertex3 0
+vertex4 1
--- vertex_cover/vertex_cover.cpp
+++ vertex_cover/vertex_cover.cpp
@@ -3,10 +3,14 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <chrono>
+#include "gurobi_c++.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
+    auto start = chrono::system_clock::now();
+
     // read
     if (argc != 2) {
         cout << "Please specify the input file name." << endl;
@@ -22,7 +26,6 @@ int main(int argc, char **argv) {
     ifs >> buf >> buf >> numVertexes;  // numVertexes : xxx
     getline(ifs, buf);
     vector<vector<int>> adjLists(numVertexes);
-
     for (int i = 0; i < numVertexes; ++i) {
         getline(ifs, buf);
         istringstream iss(buf);
@@ -32,6 +35,44 @@ int main(int argc, char **argv) {
             adjLists[i].push_back(adj);
         }
     }
+
+    // init Gurobi env & model
+    GRBEnv env = GRBEnv();
+    env.set(GRB_IntParam_LogToConsole, 0);  // make the console silent
+    GRBModel model = GRBModel(env);
+
+    // variables
+    vector<GRBVar> vertexVars(numVertexes);
+    for (int i = 0; i < numVertexes; ++i) {
+        vertexVars[i] = model.addVar(0.0, 1.0, 1.0, GRB_BINARY, "vertex" + to_string(i));
+    }
+
+    // constraints
+    for (int i = 0; i < numVertexes; ++i) {
+        for (int j : adjLists[i]) {
+            if (i < j) {
+                model.addConstr(vertexVars[i] + vertexVars[j] >= 1, "edge" + to_string(i) + "_" + to_string(j));
+            }
+        }
+    }
+
+    model.optimize();
+    cout << "Min vertex cover is " << model.getObjective().getValue() << endl;
+    cout << "Selected vertexes are: ";
+    for (int i = 0; i < numVertexes; ++i) {
+        int value = vertexVars[i].get(GRB_DoubleAttr_X);
+        if (value == 1) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    // debug file
+    model.write("vertex_cover.lp");
+    model.write("vertex_cover.sol");
+
+    chrono::duration<double> duration = chrono::system_clock::now() - start;
+    cout << "The program takes " << duration.count() << " seconds." << endl;
 
     return 0;
 }
